8f7c035c8c057e1b65017dcfa5a7abcb2d4177d1
崔晓川 <316432574@qq.com>
2022-06-15 22:46:03 +0800

    事务增加内部异常回滚机制，弃用 TransactionScope 同步扩展方法

---
+++ Dapper.Easies/TransactionRollback.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Dapper.Easies
+{
+    public interface IRollback
+    {
+        void Rollback();
+    }
+
+    public interface IRollbackWithResult
+    {
+        void Rollback<T>(T returnValue);
+    }
+
+    public sealed class TransactionRollback : IRollback, IRollbackWithResult
+    {
+        internal static TransactionRollback Instance { get; } = new TransactionRollback();
+
+        private TransactionRollback() { }
+
+        internal class TransactionRollbackException : Exception
+        { 
+        }
+
+        internal class TransactionRollbackException<T> : TransactionRollbackException
+        {
+            public T ReturnValue { get; }
+
+            public TransactionRollbackException(T returnValue)
+            {
+                ReturnValue = returnValue;
+            }
+        }
+
+        public static void Rollback() => throw new TransactionRollbackException();
+
+        public static void Rollback<T>(T returnValue) => throw new TransactionRollbackException<T>(returnValue);
+
+        void IRollback.Rollback() => Rollback();
+
+        void IRollbackWithResult.Rollback<T>(T returnValue) => Rollback(returnValue);
+    }
+}
